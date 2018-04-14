0a4b71cde3b80736fb6ba4bb744ed22b54613bc2
Shea Kusiak <shkusiak@gmail.com>
2018-04-14 20:31:25 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,11 @@
 class MP3Importer
+  attr_accessor :path
   #list_of_filenames = ["ainv", "lhndfk", "wfdm"]
+
+  def initialize(path)
+    @path = path
+  end
+
   def import(list_of_filenames)
     list_of_filenames.each { |filename|
     Song.new_by_filename(filename)}
