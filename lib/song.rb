1f699cafa580b345859b965d10c92a8f997a0775
Shea Kusiak <shkusiak@gmail.com>
2018-04-14 20:51:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
 
   def initialize(name)
     @name = name
-    @artist = artist
+    #@artist = artist
   end
 
   def self.new_by_filename(file_name)
@@ -16,4 +16,5 @@ class Song
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
+  end
 end
